da7c636e7b7d74755ae439addf2d945fe6d0b66b
YagoMoraess <yago.mbarreto@hotmail.com>
2021-07-23 16:55:08 -0300

    Autenticação do Usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$7VrCT3OvNjZVufzK/cOU8.JXrb7sNOQjgUK8tcP4YoY2DqO0N65mu');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
